b754adeef7e9ed80d7e7aa20600b87dbc20fa00f
tom_glb <goodMoring_glb@qq.com>
2019-04-13 14:01:58 +0800

    right git

--- src/main/java/cn/wj/controller/UserController.java
+++ src/main/java/cn/wj/controller/UserController.java
@@ -3,6 +3,6 @@ package cn.wj.controller;
 public class UserController {
 
 	public static void main(String[] args) {
-		System.out.println("hello git");
+		System.out.println("hello git  Right!!!");
 	}
 }
